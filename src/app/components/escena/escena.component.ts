8da1893fdd96825110c6c0f54eb9a3828e1dbed9
franbcn93 <franciscoparisalbero@gmail.com>
2022-01-28 13:17:04 +0100

    mofifications done

--- src/app/components/escena/escena.component.ts
+++ src/app/components/escena/escena.component.ts
@@ -11,7 +11,6 @@ export class EscenaComponent implements OnInit {
   
   @Input() listado_frases: FrasesHistoria[] = [];
   currentSentence:number = 0;
-  image:string = "1.jpg";
 
   constructor() {
    }
@@ -21,23 +20,18 @@ export class EscenaComponent implements OnInit {
   
 
   next(){
-    console.log(this.listado_frases[this.currentSentence].img, this.image);
     if(this.currentSentence === 3){
       this.currentSentence = 0;
-      this.image = "1.jpg";
     }else{
       this.currentSentence++;
-      this.image = this.listado_frases[this.currentSentence].img;
     }
   }
 
   prev(){
     if(this.currentSentence === 0){
       this.currentSentence = 3;
-      this.image = this.listado_frases[3].img;
     }else{
       this.currentSentence--;
-      this.image = this.listado_frases[this.currentSentence].img;  
     }
   }
 
